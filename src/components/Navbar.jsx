be3d2134da74368db54aaa9c3d40958c34739176
noman9568 <noman.22b0131078@abes.ac.in>
2025-06-22 19:26:45 +0530

    22-06

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 import "../styles/navbar.css";
+import { RxHamburgerMenu } from "react-icons/rx";
 const Navbar = () =>{
   return (
     <div className="navbar-container">
       <div className="name">Noman Hussain</div>
       <div className="nav">
-        
         <div className="list">
           <div className="list-tag">HOME</div>
           <div className="list-tag">PROJECTS</div>
@@ -14,6 +14,10 @@ const Navbar = () =>{
       </div>
       <div className="contact">
         Contact Me
+      </div>
+      <RxHamburgerMenu className="sidebar-icon"/>
+      <div className="sidebar">
+
       </div>
     </div>
   )
